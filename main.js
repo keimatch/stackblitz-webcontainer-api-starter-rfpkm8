dbd450f60390bf240c6b7fc7b143eea70715727d
keisuke machida <key.calling24@gmail.com>
2024-03-16 12:00:15 +0900

    xtermとxterm-addon-fitを追加しました

--- main.js
+++ main.js
@@ -1,35 +1,45 @@
-import './style.css';
-import { WebContainer } from '@webcontainer/api';
-import { files } from './files';
+import "./style.css";
+import { WebContainer } from "@webcontainer/api";
+import { files } from "./files";
+import { Terminal } from "xterm";
+import { FitAddon } from "xterm-addon-fit";
+import "xterm/css/xterm.css";
 
 /** @type {import('@webcontainer/api').WebContainer}  */
 let webcontainerInstance;
 
-window.addEventListener('load', async () => {
-  textareaEl.value = files['index.js'].file.contents;
-  textareaEl.addEventListener('input', (e) => {
+window.addEventListener("load", async () => {
+  textareaEl.value = files["index.js"].file.contents;
+  textareaEl.addEventListener("input", (e) => {
     writeIndexJS(e.currentTarget.value);
   });
+
+  const fitAddon = new FitAddon();
+  const terminal = new Terminal({
+    convertEol: true,
+  });
+  terminal.open(terminalEl);
+  fitAddon.fit();
 
   // Call only once
   webcontainerInstance = await WebContainer.boot();
   await webcontainerInstance.mount(files);
 
-  const exitCode = await installDependencies();
+  const exitCode = await installDependencies(terminal);
   if (exitCode !== 0) {
-    throw new Error('Installation failed');
+    throw new Error("Installation failed");
   }
 
-  startDevServer();
+  startDevServer(terminal);
 });
 
-async function installDependencies() {
+async function installDependencies(terminal) {
   // Install dependencies
-  const installProcess = await webcontainerInstance.spawn('npm', ['install']);
+  const installProcess = await webcontainerInstance.spawn("npm", ["install"]);
   installProcess.output.pipeTo(
     new WritableStream({
       write(data) {
-        console.log(data);
+        terminal.write(data);
       },
     })
   );
@@ -37,15 +47,17 @@ async function installDependencies() {
   return installProcess.exit;
 }
 
-async function startDevServer() {
+async function startDevServer(terminal) {
   // Run `npm run start` to start the Express app
-  await webcontainerInstance.spawn('npm', ['run', 'start']);
+  await webcontainerInstance.spawn("npm", ["run", "start"]);
 
   // Wait for `server-ready` event
-  webcontainerInstance.on('server-ready', (port, url) => {
-    console.log('here');
+  webcontainerInstance.on("server-ready", (port, url) => {
+    console.log("here");
     iframeEl.src = url;
   });
+
+  startShell(terminal);
 }
 
 /**
@@ -53,10 +65,42 @@ async function startDevServer() {
  */
 
 async function writeIndexJS(content) {
-  await webcontainerInstance.fs.writeFile('/index.js', content);
+  await webcontainerInstance.fs.writeFile("/index.js", content);
 }
 
-document.querySelector('#app').innerHTML = `
+/**
+ * @param {Terminal} terminal
+ */
+async function startShell(terminal) {
+  const shellProcess = await webcontainerInstance.spawn("jsh", {
+    terminal: {
+      cols: terminal.cols,
+      rows: terminal.rows,
+    },
+  });
+  shellProcess.output.pipeTo(
+    new WritableStream({
+      write(data) {
+        terminal.write(data);
+      },
+    })
+  );
+  const input = shellProcess.input.getWriter();
+  window.addEventListener("resize", () => {
+    fitAddon.fit();
+    shellProcess.resize({
+      cols: terminal.cols,
+      rows: terminal.rows,
+    });
+  });
+  terminal.onData((data) => {
+    input.write(data);
+  });
+
+  return shellProcess;
+}
+
+document.querySelector("#app").innerHTML = `
   <div class="container">
     <div class="editor">
       <textarea>I am a textarea</textarea>
@@ -65,10 +109,14 @@ document.querySelector('#app').innerHTML = `
       <iframe src="loading.html"></iframe>
     </div>
   </div>
+  <div class="terminal"></div>
 `;
 
 /** @type {HTMLIFrameElement | null} */
-const iframeEl = document.querySelector('iframe');
+const iframeEl = document.querySelector("iframe");
 
 /** @type {HTMLTextAreaElement | null} */
-const textareaEl = document.querySelector('textarea');
+const textareaEl = document.querySelector("textarea");
+
+/** @type {HTMLTextAreaElement | null} */
+const terminalEl = document.querySelector(".terminal");
